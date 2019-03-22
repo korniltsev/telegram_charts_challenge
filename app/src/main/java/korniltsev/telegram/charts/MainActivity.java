c9830cbcc560857665bec49f3c9583fc8e7d0269
Anatoly Korniltsev <korniltsev.anatoly@gmail.com>
2019-03-23 02:59:32 +0300

    tmp

--- app/src/main/java/korniltsev/telegram/charts/MainActivity.java
+++ app/src/main/java/korniltsev/telegram/charts/MainActivity.java
@@ -78,13 +78,20 @@ public class MainActivity extends Activity {
     private ImageView imageButton;
     private ChartViewGL chart_;
     private MyContentRoot root;
+    private FrameLayout contentFrame;
+    private View sgadow;
+    private View currentView;
+    private ChartData[] data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dimen = new Dimen(this);
 
-        ChartData[] data = readData();
-//        if (LOGGING) Log.d(TAG, "data len " + data.length);
+//        long t1 = SystemClock.elapsedRealtimeNanos();
+        data = readData();//todo make async
+//        long t2 = SystemClock.elapsedRealtimeNanos();
+//        Log.d(TAG, "reading " + (t2 - t1)/1000000f);
 
         ChartData datum = data[DATASET];
 //        ChartData datum = new ChartData(new ColumnData[]{
@@ -92,15 +99,103 @@ public class MainActivity extends Activity {
 //                new ColumnData("Y1", "Y1", new long[]{5, 10, 5}, "line",  MyColor.red),
 //                new ColumnData("Y2", "Y2", new long[]{2, 20, 2}, "line",  MyColor.green),
 //        });
-        if (DEBUG) {
-//            ColumnData vs = datum.data[datum.data.length - 1];
-//            vs.minValue = 0;
-//            vs.values[vs.values.length - 1] = 0;
+
+
+        prepareRootView();
+
+        createChartList();
+//        createChart(datum);
+
+
+    }
+
+    private void createChartList() {
+        ScrollView.LayoutParams listLP = new ScrollView.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+        LinearLayout list = new LinearLayout(this);
+        list.setOrientation(LinearLayout.VERTICAL);
+        list.setLayoutParams(listLP);
+        list.setPadding(0, dimen.dpi(8), 0, dimen.dpi(8));
+        for (int i = 0; i < 5; i++) {
+            TextView v = new TextView(this);
+            v.setText("Chart " + (i + 1));
+            v.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+            v.setPadding(dimen.dpi(16), 0, 0, 0);
+            list.addView(v, MATCH_PARENT, dimen.dpi(50));
+        }
+//        list.addView(legend);
+//        list.addView(chart_);
+
+        ScrollView scrollView = new ScrollView(this);
+        scrollView.addView(list);
+        mySetContentVie(scrollView);
+
+    }
+
+    private void createChart(ChartData datum) {
+        LinearLayout.LayoutParams legendLP = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+        TextView legend = new TextView(this);
+        legend.setPadding(dimen.dpi(16), dimen.dpi(8), 0, dimen.dpi(8));
+        legend.setTextSize(16f);
+        legend.setTextColor(currentColorSet.legendTitle);
+        legend.setText("Followers");
+        legend.setLayoutParams(legendLP);
+        MyColorDrawable d1 = new MyColorDrawable(currentColorSet.lightBackground, false);
+        ds.add(d1);
+        legend.setBackgroundDrawable(d1);
+
+
+        chart_ = new ChartViewGL(this, datum, dimen, currentColorSet);
+        chart_.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+
+
+        ScrollView.LayoutParams listLP = new ScrollView.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+        LinearLayout list = new LinearLayout(this);
+        list.setOrientation(LinearLayout.VERTICAL);
+        list.setLayoutParams(listLP);
+        list.addView(legend);
+        list.addView(chart_);
+
+        ScrollView scrollView = new ScrollView(this);
+        scrollView.addView(list);
+
+        ColumnData[] data1 = datum.data;
+        for (int i = 0, data1Length = data1.length; i < data1Length; i++) {
+            final ColumnData c = data1[i];
+            if (c.id.equals(ChartData.COLUMN_ID_X)) {
+                continue;
+            }
+            CheckBox cb = new CheckBox(this);
+            MyColorDrawable d = new MyColorDrawable(currentColorSet.lightBackground, i == 1);
+            ds.add(d);
+            cb.setBackgroundDrawable(d);
+            cb.setText(c.name);
+            cb.setChecked(true);
+            cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    chart_.setChecked(c.id, isChecked);
+                }
+            });
+            LinearLayout.LayoutParams cblp = new LinearLayout.LayoutParams(MATCH_PARENT, dimen.dpi(50));
+            cblp.bottomMargin = i == 0 ? 0 : dimen.dpi(1);//todo this is wrong, it is not full width
+            cb.setLayoutParams(cblp);
+
+            list.addView(cb);
+
         }
 
-        dimen = new Dimen(this);
+        mySetContentVie(scrollView);
+    }
 
-//        bgRoot = new MyColorDrawable(currentColorSet.darkBackground, false);
+    private void mySetContentVie(View v) {
+        if (currentView != null) {
+            contentFrame.removeView(currentView);
+        }
+        currentView = v;
+        contentFrame.addView(v, 0);
+    }
+
+    private void prepareRootView() {
         final int toolbar_size = dimen.dpi(56);
 
 
@@ -140,113 +235,20 @@ public class MainActivity extends Activity {
         toolbar.addView(imageButton);
 
 
-        LinearLayout.LayoutParams legendLP = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
-        TextView legend = new TextView(this);
-        legend.setPadding(dimen.dpi(16), dimen.dpi(8), 0, dimen.dpi(8));
-        legend.setTextSize(16f);
-        legend.setTextColor(currentColorSet.legendTitle);
-        legend.setText("Followers");
-        legend.setLayoutParams(legendLP);
-        MyColorDrawable d1 = new MyColorDrawable(currentColorSet.lightBackground, false);
-        ds.add(d1);
-        legend.setBackgroundDrawable(d1);
-
-
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
-        chart_ = new ChartViewGL(this, datum, dimen, currentColorSet);
-        chart_.setLayoutParams(lp);
-
-
-        ScrollView.LayoutParams listLP = new ScrollView.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
-        LinearLayout list = new LinearLayout(this);
-        list.setOrientation(LinearLayout.VERTICAL);
-        list.setLayoutParams(listLP);
-        list.addView(legend);
-        list.addView(chart_);
-
-        ScrollView scrollView = new ScrollView(this) ;
-//        {
-//            @Override
-//            public void requestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-//                Log.d("ScrollBug", "requestDisallowInterceptTouchEvent");
-//
-//                super.requestDisallowInterceptTouchEvent(disallowIntercept);
-//                getParent().requestDisallowInterceptTouchEvent(disallowIntercept);
-////                System.out.println();
-//            }
-//
-//            @Override
-//            public boolean onInterceptTouchEvent(MotionEvent ev) {
-//                boolean res = super.onInterceptTouchEvent(ev);
-//                Log.d("ScrollBug", "onInterceptTouchEvent " + MotionEvent.actionToString(ev.getActionMasked()) + " " + res);
-//                return res;
-//            }
-//
-//            @Override
-//            public boolean onTouchEvent(MotionEvent ev) {
-//                boolean re = super.onTouchEvent(ev);
-//                Log.d("ScrollBug", "onTouchEvent " + MotionEvent.actionToString(ev.getActionMasked()) + " " + re);
-//
-//                return re;
-//            }
-//
-//            @Override
-//            public boolean dispatchTouchEvent(MotionEvent ev) {
-//                boolean e = super.dispatchTouchEvent(ev);
-//                Log.d("ScrollBug", "dispatchTouchEvent " + MotionEvent.actionToString(ev.getActionMasked()) + " " + e);
-//
-//                return e;
-//            }
-//        };
-        scrollView.addView(list);
-
         LinearLayout.LayoutParams frameLP = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
-        FrameLayout contentFrame = new FrameLayout(this);
+        contentFrame = new FrameLayout(this);
         contentFrame.setLayoutParams(frameLP);
-//        contentFrame.addView(scrollView, MATCH_PARENT, dimen.dpi(400));
-        contentFrame.addView(scrollView, MATCH_PARENT, MATCH_PARENT);
-        View sgadow = new View(this);
+        sgadow = new View(this);
         sgadow.setBackgroundDrawable(getResources().getDrawable(R.drawable.header_shadow));
         contentFrame.addView(sgadow, MATCH_PARENT, dimen.dpi(3));
 
         root = new MyContentRoot(this, currentColorSet.statusbar, currentColorSet.darkBackground);
         root.setOrientation(LinearLayout.VERTICAL);
-        root.addView(toolbar, lp);
+        root.addView(toolbar, new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
         root.addView(contentFrame);
 
-        ColumnData[] data1 = datum.data;
-        for (int i = 0, data1Length = data1.length; i < data1Length; i++) {
-            final ColumnData c = data1[i];
-            if (c.id.equals(ChartData.COLUMN_ID_X)) {
-                continue;
-            }
-            CheckBox cb = new CheckBox(this);
-            MyColorDrawable d = new MyColorDrawable(currentColorSet.lightBackground, i == 1);
-            ds.add(d);
-            cb.setBackgroundDrawable(d);
-            cb.setText(c.name);
-            cb.setChecked(true);
-            cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    chart_.setChecked(c.id, isChecked);
-                }
-            });
-            LinearLayout.LayoutParams cblp = new LinearLayout.LayoutParams(MATCH_PARENT, dimen.dpi(50));
-            cblp.bottomMargin = i == 0? 0:dimen.dpi(1);//todo this is wrong, it is not full width
-            cb.setLayoutParams(cblp);
-
-            list.addView(cb);
-
-        }
 
         if (Build.VERSION.SDK_INT >= 21) {
-//            root.isScrollContainer()
-//            root.requestApplyInsets();
-//            //todo
-//            root.setFitsSystemWindows(false);
-
-
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -254,7 +256,6 @@ public class MainActivity extends Activity {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(root);
-
     }
 
     final Handler h = new Handler();
@@ -275,7 +276,9 @@ public class MainActivity extends Activity {
         for (MyColorDrawable d : ds) {
             d.animate(currentColorSet.lightBackground);
         }
-        chart_.animateToColors(currentColorSet);
+        if (chart_ != null) {
+            chart_.animateToColors(currentColorSet);
+        }
         h.removeCallbacksAndMessages(null);
         h.postDelayed(new Runnable() {
             @Override
@@ -300,7 +303,7 @@ public class MainActivity extends Activity {
         if (TRACE) {
             File filesDir = getFilesDir();
             File trace = new File(filesDir, "trace");
-            Debug.startMethodTracing(trace.getAbsolutePath(), 1024  * 1024 * 10);
+            Debug.startMethodTracing(trace.getAbsolutePath(), 1024 * 1024 * 10);
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
@@ -344,6 +347,7 @@ public class MainActivity extends Activity {
     }
 
     public static final boolean USE_RIPPLE = true;
+
     public final Drawable createButtonBackground(int pressedColor) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP || !USE_RIPPLE) {
             StateListDrawable stateListDrawable = new StateListDrawable();
@@ -398,12 +402,11 @@ public class MainActivity extends Activity {
             }
         }
 
-        public void animateColors(int status, int bg){
+        public void animateColors(int status, int bg) {
             animStatus = new MyAnimation.Color(MyAnimation.ANIM_DRATION, this.colorStatusbar, status);
             animBg = new MyAnimation.Color(MyAnimation.ANIM_DRATION, this.colorBackground, bg);
             invalidate();
         }
-
 
 
         @Override
